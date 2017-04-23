d38f953e331f4488f879ffa4379c836b2eccc5ef
devinjoseph <devin.joseph@yahoo.com>
2017-04-22 22:31:48 -0700

    Basic Turret
    The basic turret

---
+++ BeachHacksGame/Assets/TurretScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour {
+    public Transform fire;
+    public GameObject bullet;
+
+    private int timer;
+    [SerializeField]
+    private int fireRate;
+    // Use this for initialization
+    void Start () {
+        timer = 0;
+        fireRate = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 1;
+       
+        if (timer * Time.deltaTime > fireRate)
+        {
+            Instantiate(bullet, fire.position, fire.rotation);
+            timer = 0;
+        }
+    }
+}
