704598b7937d8b85054202aeffea96c242ae073c
Matthew Lee <mlee1@cpp.edu>
2017-04-22 23:19:43 -0700

    Enemy spawner
    Script and prefab that spawns enemies

---
+++ BeachHacksGame/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+    public GameObject Enemy;
+    public float numEnemy;
+    public float startTime;
+    public float waitTime;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnWaves());
+	}
+	
+
+    IEnumerator SpawnWaves()
+    {
+        while(true)
+        {
+            for(int i = 0; i < numEnemy; i++)
+            {
+                Instantiate(Enemy, transform.position, Quaternion.identity);
+                Debug.Log("Enemy Spawned!");
+
+            }
+
+            yield return new WaitForSeconds (waitTime);
+        }
+    }
+}
