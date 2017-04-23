3f800136a65073caf8ef8f193b6bbc9f821f8b54
Matthew Lee <mlee1@cpp.edu>
2017-04-23 04:15:00 -0700

    main menu
    very basic main menu

---
+++ BeachHacksGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
