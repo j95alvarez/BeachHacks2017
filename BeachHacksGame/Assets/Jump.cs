06cf552a76ae7d04d6cea2b1492c773adec9ec5a
j95alvarez <juanalvarez391@yahoo.com>
2017-04-22 23:06:48 -0700

    afafa

---
+++ BeachHacksGame/Assets/Jump.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    //Animator animatorObj;
+
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float groundRadius;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool isGrounded, _jump;
+
+    [SerializeField]
+    private float jumpForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        //animatorObj = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           // animatorObj.Play("Player_jump");
+            _jump = true;
+        }
+
+        if (IsGrounded() && _jump)
+        {
+            _jump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
