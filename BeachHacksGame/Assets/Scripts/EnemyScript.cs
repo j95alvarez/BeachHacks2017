67dbe9dca2326baeb09866b6ed34af18cbd3d4bc
j95alvarez <juanalvarez391@yahoo.com>
2017-04-22 21:32:20 -0700

    Added Dev and Neil's stuff

---
+++ BeachHacksGame/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    public GameObject player;
+    public float MoveSpeed;
+    public const float maxHp = 20;
+    public float hp;
+
+    // Use this for initialization
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(player.tag);
+        hp = maxHp;
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        if (player != null) {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MoveSpeed * Time.deltaTime);
+        }
+
+        if (hp <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+}
