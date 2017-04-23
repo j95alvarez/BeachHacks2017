813c1ca5df1d0f4ad418e48ef7cb0d708ca995bd
nkawong <Neil@dhcp-39-84-56.dhcpint.csulb.edu>
2017-04-22 19:26:00 -0700

    enemy movement
    movement for enemy

---
+++ BeachHacksGame/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+	public const float maxHp = 20;
+	public float hp;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		Debug.Log (player.tag);
+		hp = maxHp;
+	}
+	public GameObject player;
+	public float MoveSpeed = 1;
+	int maxDist = 10;
+	int minDist = 2;
+	// Update is called once per frame
+	void Update () {
+		if (player!=null) {
+			transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MoveSpeed * Time.deltaTime);
+		}
+		if (hp <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
